ec707d5fd32584b209609a3f50e260537fbaacb3
pozdnyakov-as <Pozdnyakov-AS@gaz-is.ru>
2017-12-02 22:28:27 +0300

    design pattern. Add examples annotations

---
+++ dessign_pattern/src/main/java/com/t0lia/dessign_pattern/core/Bad.java
@@ -0,0 +1,10 @@
+package com.t0lia.dessign_pattern.core;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Bad { }
