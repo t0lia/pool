68de02ec0ab54744904332e96226590ff5c3765e
pozdnyakov-as <Pozdnyakov-AS@gaz-is.ru>
2017-12-11 21:21:25 +0300

    desing pattern. Adding description to abstract factory and factory methdo

--- dessign_pattern/src/main/java/com/t0lia/design_pattern/I_creational/abstract_factory/AbstractFactory.java
+++ dessign_pattern/src/main/java/com/t0lia/design_pattern/I_creational/abstract_factory/AbstractFactory.java
@@ -1,31 +1,45 @@
 package com.t0lia.design_pattern.I_creational.abstract_factory;
 
+import com.t0lia.design_pattern.I_creational.factory_method.FactoryMethod;
+
 /**
  * <strong>Description</strong>
  * <p>
+ * Creates groups of objects without specified concrete classes. Used in plugin architecture
+ * Every class of that group represents as {@link FactoryMethod}. Client work with concrete instance
+ * of AbstractFactory using interface and doesn't know which concrete instance use.
  * <p>
  * <pre class="code">
  * <strong>Pros:</strong>
- * <ol></ol>
+ * <ul>
+ *     <li>Application become independent of how it object created</li>
+ *     <li>Class become independent of how objects it required created</li>
+ *     <li>Creating families of related or dependent objects</li>
+ * </ul>
+ * <p>
  * <strong>Cons:</strong>
+ * <ul>
  * <ol></ol>
+ * </ul>
+ * <p>
  * <strong>Example:</strong>
  * <pre class="code"><code class="java">
  * </code></pre>
  * <p>
  * <strong>Books</strong>
  * <ul>
- *     <li>Effective Java, Joshua Bloch</li>
- *     <li>Design Pattern, GOF</li>
+ * <li>Effective Java, Joshua Bloch</li>
+ * <li>Design Pattern, GOF</li>
  * </ul>
- *
+ * <p>
  * <string>Related patterns</string>
- * <ul></ul>
+ * <ul>{@link FactoryMethod} - simple case with only one provided class</ul>
  * <strong>Improvements</strong>
  * <p>
  * <strong>JDK examples:</strong>
- * <ul></ul>
+ * <ul><li>jdbc connection</li></ul>
  * <p>
-**/
-public interface AbstractFactory {}
+ **/
+public interface AbstractFactory {
+}
 
--- dessign_pattern/src/main/java/com/t0lia/design_pattern/I_creational/factory_method/FactoryMethod.java
+++ dessign_pattern/src/main/java/com/t0lia/design_pattern/I_creational/factory_method/FactoryMethod.java
@@ -1,13 +1,24 @@
 package com.t0lia.design_pattern.I_creational.factory_method;
+
+import com.t0lia.design_pattern.I_creational.abstract_factory.AbstractFactory;
+
+import java.util.Calendar;
+
 /**
  * <strong>Description</strong>
  * <p>
+ * Create a object without specifying exact class. This method can be overridden rather that calling constructor
  * <p>
  * <pre class="code">
  * <strong>Pros:</strong>
- * <ol></ol>
+ * <ul>
+ *      <li>Child classes can override method rather than constructor</li>
+ * </ul>
  * <strong>Cons:</strong>
+ * <ul>
  * <ol></ol>
+ * </ul>
+ * <p>
  * <strong>Example:</strong>
  * <pre class="code"><code class="java">
  * </code></pre>
@@ -19,11 +30,12 @@ package com.t0lia.design_pattern.I_creational.factory_method;
  * </ul>
  *
  * <string>Related patterns</string>
- * <ul></ul>
+ * <ul><li>{@link AbstractFactory} - creates a group of related object. Each creation method is a factory method</li></ul>
  * <strong>Improvements</strong>
  * <p>
  * <strong>JDK examples:</strong>
- * <ul></ul>
+ * <ul>{@link Calendar#getInstance()}</ul>
+ * <ul>{@link java.util.EnumSet#of()}</ul>
  * <p>
  */
 public interface FactoryMethod {
